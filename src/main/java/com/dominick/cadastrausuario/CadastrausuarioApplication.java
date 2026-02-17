ef6c2ee682bf790d9c3a7c0951863c0a31b369ad
Jhonathan Dominick <jhonathandominickdemelo@gmail.com>
2026-02-17 08:33:55 -0300

    Commit inicial

---
+++ src/main/java/com/dominick/cadastrausuario/CadastrausuarioApplication.java
@@ -0,0 +1,13 @@
+package com.dominick.cadastrausuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrausuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrausuarioApplication.class, args);
+	}
+
+}
